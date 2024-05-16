dbc5e66ae59f6204686c2febe1d793ce261cd058
quochn-RisingStar <103491433+quochn-RisingStar@users.noreply.github.com>
2024-05-16 08:54:23 +0700

    Update UIImagesToVideo.swift

--- Sources/SwiftUIViewRecorder/Video/UIImagesToVideo.swift
+++ Sources/SwiftUIViewRecorder/Video/UIImagesToVideo.swift
@@ -46,72 +46,72 @@ extension Array where Element == UIImage {
      - Returns: Future URL of a generated video file or Error
      */
     func toVideo(framesPerSecond: Double,
-                 codecType: AVVideoCodecType = .h264) -> Future<URL?, Error> {
-        print("Generating video framesPerSecond=\(framesPerSecond), codecType=\(codecType.rawValue)")
+             codecType: AVVideoCodecType = .h264) -> Future<URL?, Error> {
+    print("Generating video framesPerSecond=\(framesPerSecond), codecType=\(codecType.rawValue)")
+
+    return Future<URL?, Error> { promise in
+        guard self.count > 0 else {
+            promise(.failure(UIImagesToVideoError.noFrames))
+            return
+        }
+
+        guard framesPerSecond > 0 else {
+            promise(.failure(UIImagesToVideoError.invalidFramesPerSecond))
+            return
+        }
+
+        let url = self.makeUniqueTempVideoURL()
+
+        let writer: AVAssetWriter
+        do {
+            writer = try AVAssetWriter(outputURL: url, fileType: .mov)
+        } catch {
+            promise(.failure(error))
+            return
+        }
         
-        return Future<URL?, Error> { promise in
-            guard self.count > 0 else {
-                promise(.failure(UIImagesToVideoError.noFrames))
-                return
-            }
-            
-            guard framesPerSecond > 0 else {
-                promise(.failure(UIImagesToVideoError.invalidFramesPerSecond))
-                return
-            }
-            
-            let url = self.makeUniqueTempVideoURL()
-            
-            let writer: AVAssetWriter
-            do {
-                writer = try AVAssetWriter(outputURL: url, fileType: .mov)
-            } catch {
-                promise(.failure(error))
-                return
-            }
-                        
-            let input = AVAssetWriterInput(mediaType: .video,
-                                           outputSettings: self.videoSettings(codecType: codecType))
-                                    
-            if (writer.canAdd(input)) {
-                writer.add(input)
-            } else {
-                promise(.failure(UIImagesToVideoError.internalError))
-                return
-            }
-            
-            let pixelAdaptor = AVAssetWriterInputPixelBufferAdaptor(assetWriterInput: input,
-                                                                    sourcePixelBufferAttributes: pixelAdaptorAttributes)
-            
-            writer.startWriting()
-            writer.startSession(atSourceTime: CMTime.zero)
-            
-            var frameIndex: Int = 0
-            while frameIndex < self.count {
-                if (input.isReadyForMoreMediaData) {
-                    if let buffer = self[frameIndex].toSampleBuffer(frameIndex: frameIndex,
-                                                                    framesPerSecond: framesPerSecond) {
-                        pixelAdaptor.append(CMSampleBufferGetImageBuffer(buffer)!,
-                                            withPresentationTime: CMSampleBufferGetOutputPresentationTimeStamp(buffer))
+        let input = AVAssetWriterInput(mediaType: .video, outputSettings: self.videoSettings(codecType: codecType))
+
+        if writer.canAdd(input) {
+            writer.add(input)
+        } else {
+            promise(.failure(UIImagesToVideoError.internalError))
+            return
+        }
+
+        let pixelAdaptor = AVAssetWriterInputPixelBufferAdaptor(assetWriterInput: input, sourcePixelBufferAttributes: pixelAdaptorAttributes)
+
+        writer.startWriting()
+        writer.startSession(atSourceTime: CMTime.zero)
+
+        let frameDuration = CMTimeMake(value: 1, timescale: Int32(framesPerSecond))
+        var frameIndex: Int = 0
+
+        input.requestMediaDataWhenReady(on: DispatchQueue(label: "mediaInputQueue")) {
+            while input.isReadyForMoreMediaData, frameIndex < self.count {
+                autoreleasepool {
+                    if let buffer = self[frameIndex].toSampleBuffer(frameIndex: frameIndex, framesPerSecond: framesPerSecond) {
+                        let presentationTime = CMTimeMultiply(frameDuration, multiplier: Int32(frameIndex))
+                        pixelAdaptor.append(CMSampleBufferGetImageBuffer(buffer)!, withPresentationTime: presentationTime)
                     }
-                    
                     frameIndex += 1
                 }
             }
-        
-            writer.finishWriting {
-                switch writer.status {
-                case .completed:
-                    print("Successfully finished writing video \(url)")
-                    promise(.success(url))
-                    break
-                default:
-                    let error = writer.error ?? UIImagesToVideoError.internalError
-                    print("Finished writing video without success \(error)")
-                    promise(.failure(error))
+
+            if frameIndex >= self.count {
+                input.markAsFinished()
+                writer.finishWriting {
+                    switch writer.status {
+                    case .completed:
+                        print("Successfully finished writing video \(url)")
+                        promise(.success(url))
+                    default:
+                        let error = writer.error ?? UIImagesToVideoError.internalError
+                        print("Finished writing video without success \(error)")
+                        promise(.failure(error))
+                    }
                 }
             }
         }
     }
-    
 }
